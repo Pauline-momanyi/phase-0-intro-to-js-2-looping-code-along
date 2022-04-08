197e1e5bb992dd16979e6f368593c75343047989
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-08 11:14:32 +0300

    For and while loop concept

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+var sNames = []
+function writeCards(strNames, action){
+    
+    for (let i=0; i<strNames.length;i++){
+       sNames.push(`Thank you, ${strNames[i]}, for the wonderful ${action} gift!`)
+    // [...sNames,`Thank you, ${strNames[i]}, for the wonderful ${action} gift!`]
+    // return strNames.map((name)=>`Thank you, ${name}, for the wonderful ${action} gift!`
+    }
+    
+    return sNames    
+}
+
+function countDown(num){
+    let i = num;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
