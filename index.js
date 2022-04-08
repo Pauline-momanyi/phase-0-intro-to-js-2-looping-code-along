1fd948580f5916680a3145fa1a66f8069752f887
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-08 18:22:20 +0300

    Alternatives

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 var sNames = []
 function writeCards(strNames, action){
     
-    for (let i=0; i<strNames.length;i++){
-       sNames.push(`Thank you, ${strNames[i]}, for the wonderful ${action} gift!`)
+    for (let i of strNames){
+       sNames.push(`Thank you, ${i}, for the wonderful ${action} gift!`)
+    //for (let i=0; i<strNames.length:i++){
+        //sNames.push(`Thank you, ${strNames[i]}, for the wonderful ${action} gift!`)
+    //}
     // [...sNames,`Thank you, ${strNames[i]}, for the wonderful ${action} gift!`]
     // return strNames.map((name)=>`Thank you, ${name}, for the wonderful ${action} gift!`
     }
